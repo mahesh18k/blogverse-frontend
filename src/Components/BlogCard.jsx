d241f20ff11fa5437978ae6ad6cd1428906e57c0
Mahesh Kunchala <maheshkunchala18@gmail.com>
2024-07-17 23:07:07 +0530

    Update BlogCard Component

--- src/Components/BlogCard.jsx
+++ src/Components/BlogCard.jsx
@@ -4,30 +4,32 @@ import { Card, Button, Row, Col } from 'react-bootstrap';
 const BlogCard = ({ title, author, date_uploaded, thumbnail, upvotes, downvotes, view_count, onClick }) => {
   return (
     <>
-    <Card style={{ width: '18rem', margin: '1rem' }} onClick={onClick}>
-      <Card.Img variant="top" src={thumbnail} />
-      <Card.Body>
-        <Card.Title>{title}</Card.Title>
+      <Card style={{ width: '18rem', margin: '1rem' }} onClick={onClick}>
+        <Card.Img variant="top" src={thumbnail} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
 
-        <Card.Text>
-          <div>Author: {author.first_name} {author.last_name}</div>
-          <div>Uploaded: {new Date(date_uploaded).toLocaleDateString('en-IN')}</div>
-        <Row>
-          <Col>
-            <div>Upvotes: {upvotes}</div>
-          </Col>
-          <Col>
-            <div>Downvotes: {downvotes}</div>
-          </Col>
-        </Row>
-        <div>Views: {view_count}</div>
-        </Card.Text>
+          <Card.Text as="div">
+            <div>Author: {author.first_name} {author.last_name}</div>
+            <div>Uploaded: {new Date(date_uploaded).toLocaleDateString('en-IN')}</div>
+          </Card.Text>
 
-        <Button variant="primary">Read More</Button>
-      </Card.Body>
-    </Card>
+          <Row>
+            <Col>
+              <div>Upvotes: {upvotes}</div>
+            </Col>
+            <Col>
+              <div>Downvotes: {downvotes}</div>
+            </Col>
+          </Row>
+          <div>Views: {view_count}</div>
+
+          <Button variant="primary">Read More</Button>
+        </Card.Body>
+      </Card>
     </>
   );
 };
+
 
 export default BlogCard;

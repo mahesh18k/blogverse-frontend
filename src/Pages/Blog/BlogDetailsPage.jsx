01c7dc1f11f22abb0d43282c32c55642e54d80eb
Mahesh Kunchala <maheshkunchala18@gmail.com>
2024-07-22 22:53:47 +0530

    Update Loading Spinner

--- src/Pages/Blog/BlogDetailsPage.jsx
+++ src/Pages/Blog/BlogDetailsPage.jsx
@@ -28,9 +28,7 @@ const BlogDetailsPage = () => {
   if (loading) {
     return (
       <Container className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
-        <Spinner animation="border" role="status">
-          <span className="sr-only">Loading...</span>
-        </Spinner>
+        <Spinner animation="border" role="status"> </Spinner>
       </Container>
     );
   }
